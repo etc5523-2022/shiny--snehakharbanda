723c0be6b2ed56858ddfba2ca72229bb63f73e9d
Sneha Kharbanda <skha0095@student.monash.edu>
2022-08-22 23:52:20 +1000

    Ammending commit

--- app.R
+++ app.R
@@ -28,14 +28,26 @@ breaks <- hour(hm("00:00", "6:00", "12:00", "18:00", "23:59"))
 labels <- c("Night", "Morning", "Afternoon", "Evening")
 bike$Time_of_day <- cut(x=hour(bike$StartTime), breaks = breaks, labels = labels, include.lowest=TRUE)
 
-ui <- fluidPage(theme = shinytheme("darkly"),
+ui <- fluidPage(theme = shinytheme("journal"),
 
-    titlePanel("Biketown Bikeshare"),
+      titlePanel(div(column(width = 6, h2("Biketown Bikeshare Analysis")),
+                     column(width = 6, tags$img(src = "https://i.pinimg.com/originals/24/ae/8d/24ae8def288851503cf68340df174963.gif",
+                                                height="50%", width="50%", align="centre"))),
+                 ),
 
-    h3("2. Compare bike"),
+      h3("This app gives you insight into speeds of various bikes, riders in your area and general time trends. Visit the different tabs for more information!"),
+
+      tags$style(HTML("
+      .tabbable > .nav > li > a {background-color: aqua;  color:black}
+  ")),
+      tabsetPanel(
+        tabPanel(h3("Speed and Area"),
+
+
+    h3("1. Compare the average speed of the bikes."),
     fluidRow(
       sidebarLayout(
-        sidebarPanel(h1("Bike Comparison"),
+        sidebarPanel(h2("Bike Speed Comparison"),
                      selectizeInput("bikes_compare1", "Select bike 1",
                                     choices = unique(bike$bikeName)),
                      selectizeInput("bikes_compare2", "Select bike 2",
@@ -48,31 +60,37 @@ ui <- fluidPage(theme = shinytheme("darkly"),
       )
     ),
 
-    h3("3.Find Area's attributes"),
+    h3("2. Find the bikers' general attributes 10km around your Area."),
     fluidRow(
       sidebarLayout(
-        sidebarPanel(h3("Find your latitude and longitude"),
+        sidebarPanel(h3("Find your latitude and longitude if you are unsure."),
                      selectizeInput("find_latitude", "Choose your Area",
                                     choices = unique(bike$StartHub)),
                      numericInput("lat", "Enter your latitude", value = 45.52),
-                     numericInput("long", "Enter your longitude", value = -122.65)
+                     numericInput("long", "Enter your longitude", value = -122.65),
+                     actionButton("submit", "Submit")
 
         ),
 
         mainPanel(
           textOutput("latitude"),
-          tableOutput("paymentTable")
+          br(),
+          tableOutput("paymentTable"),
+          br(),
+          tableOutput("summary")
         )
       )
-    ),
+    )),
 
-    h3("What time of day do most people ride?"),
+    tabPanel(h3("Time"),
+
+    h3("3. What time of day do most people ride? Take a guess!"),
     fluidRow(
       sidebarLayout(
         sidebarPanel(
         selectizeInput("time", "Guess the time",
                      choices = unique(bike$Time_of_day)),
-        actionButton("submit", "Submit")
+        actionButton("guess", "Guess")
         ),
         mainPanel(
           textOutput("answer"),
@@ -80,9 +98,9 @@ ui <- fluidPage(theme = shinytheme("darkly"),
         )
 
       )
-    ),
+    )),
 
-
+    tabPanel(h3("About the app"),
 
     fluidRow(
       column(10,
@@ -91,7 +109,7 @@ ui <- fluidPage(theme = shinytheme("darkly"),
       )
     ),
     includeCSS("styles.css")
-)
+)))
 
 server <- function(input, output, session) {
 
@@ -103,7 +121,15 @@ server <- function(input, output, session) {
       bike %>% group_by(bikeName) %>%
         summarise(avg_speed = mean(speed, na.rm = TRUE)) %>%
         filter(bikeName == input$bikes_compare1 | bikeName == input$bikes_compare2) %>%
-        ggplot(aes(bikeName, avg_speed, fill = bikeName)) + geom_col()
+        ggplot(aes(bikeName, avg_speed, fill = bikeName)) +
+        geom_col(show.legend = FALSE) +
+        labs(title = "Average speed in miles per hour") +
+             ylab("Avg_speed in miles/hour") +
+             xlab("Bike Name") +
+        theme_minimal() +
+        theme(axis.text.x = element_text(color = "blue")) +
+        theme(text = element_text(size = 20))
+
     })
 
     output$latitude <- renderText({
@@ -116,20 +142,34 @@ server <- function(input, output, session) {
     })
 
     output$paymentTable <- renderTable({
+      if(input$submit) {
       bike1 <- bike %>%
         filter(StartLatitude == input$lat & StartLongitude == input$long) %>%
         count(PaymentPlan, sort = TRUE) %>% drop_na()
       bike1
+      }
+
+    })
+
+    output$summary <- renderTable({
+      if(input$submit) {
+      bike2 <- bike %>%
+        filter(StartLatitude == input$lat & StartLongitude == input$long) %>%
+        summarise(average_speed_milesPerHour = mean(speed, na.rm = TRUE),
+                  average_duration_seconds = mean((Duration*3600), na.rm = TRUE))
+      bike2
+      }
 
     })
 
 
     time_msg <- reactive({
+      if(input$guess){
       case_when(input$time == "Morning" ~ "This is not the correct answer. ",
       input$time == "Afternoon" ~ "This is the correct answer. ",
       input$time == "Evening" ~ "Unfortunately, you are wrong. ",
       input$time == "Night" ~ "Uhoh!. "
-      )
+      )}
     })
 
 
@@ -140,18 +180,33 @@ server <- function(input, output, session) {
         mutate(percentage = round(percentage, digits =2))
 
 
-          if(input$submit) {
+          if(input$guess) {
             glue(time_msg(), bike2$percentage[1], "% people ride in the afternoon.")
           }
     })
 
     output$Morning <- renderUI({
-      if(input$submit) {
-      tags$img(src = "https://www.pngkey.com/png/detail/54-543795_eyes-sun-cartoon-half-free-sad-rays-grumpy.png",
+      if(input$guess) {
+        if(input$time == "Morning"){
+      tags$img(src = "https://hdclipartall.com/images/good-morning-animated-clip-art-good-morning-clip-art-free-2-image-3-morning-clipart-550_400.jpg",
                height="50%", width="50%", align="centre")
-      }
+
+        }
+        else if(input$time == "Afternoon") {
+          tags$img(src = "https://www.seekpng.com/png/detail/457-4571607_happy-sun-free-download-clip-art-on-clipart.png",
+                   height="50%", width="50%", align="centre")
+        }
+        else if(input$time == "Evening") {
+          tags$img(src = "https://img.freepik.com/premium-vector/sunset-sky-cartoon-summer-sunrise-with-pink-clouds-sunshine-evening-cloudy-heaven-panorama_461812-70.jpg?w=360",
+                   height="50%", width="50%", align="centre")
+        }
+        else if(input$time == "Night") {
+          tags$img(src = "https://thumbs.dreamstime.com/b/cute-moon-character-sad-expression-cute-moon-character-sad-expression-your-commercial-project-others-182038820.jpg",
+                   height="50%", width="50%", align="centre")
+        }
+        }
     }) %>%
-      bindEvent(input$submit)
+      bindEvent(input$guess)
 
 
     output$about <- renderUI({
